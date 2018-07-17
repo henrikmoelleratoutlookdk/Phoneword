59c366834124c7529e2a0559092e73fccceed6e1
L-SE-0252\henrik <henrik.moeller@outlook.dk>
2018-07-17 16:25:33 +0200

    no message

--- Phoneword/Phoneword/MainPage.xaml.cs
+++ Phoneword/Phoneword/MainPage.xaml.cs
@@ -9,9 +9,76 @@ namespace Phoneword
 {
 	public partial class MainPage : ContentPage
 	{
-		public MainPage()
+        Entry phoneNumberText;
+        Button translateButton;
+        Button callButton;
+
+        string translatedNumber;
+
+        public MainPage()
 		{
-			InitializeComponent();
-		}
-	}
+            this.Padding = new Thickness(20, 20, 20, 20);
+
+            StackLayout panel = new StackLayout
+            {
+                Spacing = 15
+            };
+
+            panel.Children.Add(new Label
+            {
+                Text = "Enter a Phoneword:",
+                FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label))
+            });
+
+            panel.Children.Add(phoneNumberText = new Entry
+            {
+                Text = "1-855-XAMARIN",
+            });
+
+            panel.Children.Add(translateButton = new Button
+            {
+                Text = "Translate"
+            });
+
+            panel.Children.Add(callButton = new Button
+            {
+                Text = "Call",
+                IsEnabled = false,
+            });
+
+            translateButton.Clicked += OnTranslate;
+
+            this.Content = panel;
+
+        }
+
+        private void OnTranslate(object sender, EventArgs e)
+        {
+            string enteredNumber = phoneNumberText.Text;
+            translatedNumber = core_PhonewordTranslator.PhonewordTranslator.ToNumber(enteredNumber);
+
+            if (!string.IsNullOrEmpty(translatedNumber))
+            {
+                callButton.IsEnabled = true;
+                callButton.Text = "Call " + translatedNumber;
+            }
+            else
+            {
+                callButton.IsEnabled = false;
+                callButton.Text = "Call";
+            }
+        }
+
+#if __rem__
+            //InitializeComponent();
+
+            Content = new StackLayout
+            {
+                VerticalOptions = LayoutOptions.Center,
+                Children = { new Label {
+                    HorizontalTextAlignment = TextAlignment.Center,
+                    Text = "Hej Henrik: Welcome to Xamarin.Forms!"  }  }
+            };
+#endif
+    }
 }
